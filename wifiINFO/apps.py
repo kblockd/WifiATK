7a0402803752859b132ded88114fa63f5a386da4
kblockd <kblockd@gmail.com>
2022-11-05 17:33:57 +0800

    新增初始化配置数据库

--- wifiINFO/apps.py
+++ wifiINFO/apps.py
@@ -1,8 +1,18 @@
-import django.apps
+# import django.apps
 from django.apps import AppConfig
 from django.utils.module_loading import autodiscover_modules
+from django.db.models.signals import post_migrate
 import os
 import sys
+
+
+def init_data():
+    from wifiINFO.models import Conf
+    if Conf.objects.count() == 0:
+        conf_data = {"id": 1, "MONFACE": None, "ATKFACE": None,
+                    "LOGNAME": None, "LOGDIR": None, "MAIN_STATUS": 0, "ATK_STATUS": 0,
+        }
+        Conf.objects.create(**conf_data)
 
 
 class WifiinfoConfig(AppConfig):
@@ -16,10 +26,10 @@ class WifiinfoConfig(AppConfig):
         if run_once is not None:
             return
 
-        if os.path.exists('run.pid'):
-            return
-
         os.environ['CMDLINERUNNER_RUN_ONCE'] = 'True'
 
+        post_migrate.connect(init_data, sender=self)
+
+        """规避其他功能执行"""
         if 'runserver' in sys.argv:
             autodiscover_modules('start.py')

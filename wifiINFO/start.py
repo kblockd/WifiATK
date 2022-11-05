bed9cf3391a432d4eaae5ac9c3a6382968f34328
kblockd <kblockd@gmail.com>
2022-11-05 13:03:55 +0800

    删除部分无效注释

--- wifiINFO/start.py
+++ wifiINFO/start.py
@@ -4,29 +4,13 @@ import os
 import time
 
 def first_start(LOGNAME):
-    # config._init()
     config.set_value('LOGDIR', os.getcwd())
     config.set_value('LOGNAME', LOGNAME)
-    #config.set_value('LOG', 'meituan-01.csv')
 
-    LOGNAME = config.get_value('LOGNAME')
-    #LOG = LOGNAME+'-01.csv'
     time1 = time.time()
-    pid = start_airmon()
-    print(pid)
-    time2 = time.time()
-    print(time2 - time1)
 
-    # count = 0
-    # while True:
-    #     time.sleep(60)
-    #     time1 = time.time()
-    #     wifi_lines, station_lines = file_parse(LOG)
-    #     data_wifi(wifi_lines)
-    #     data_station(station_lines)
-    #     count += 1
-    #     print(count)
-    #     time2 = time.time()
-    #     print('消耗:{}'.format(time2-time1))
+    pid = start_airmon()
+    time2 = time.time()
+    print('启动成功，消耗时间：{}'.format(time2-time1))
 
 first_start('TEST123')

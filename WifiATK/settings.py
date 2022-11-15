40cba521fb7f06cc4a1c11e2816a8e020a19960e
kblockd <kblockd@gmail.com>
2022-11-15 20:15:22 +0800

    修复bug

--- WifiATK/settings.py
+++ WifiATK/settings.py
@@ -38,12 +38,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_crontab',
+    'corsheaders',
     'wifiINFO',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
